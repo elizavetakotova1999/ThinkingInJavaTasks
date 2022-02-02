62e2ecb3a3e9eb2710a125960af7135935881cb4
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-03 00:07:13 +0200

    added BitManipulation; Page 111-112;

---
+++ src/com/examples/operators/BitManipulation.java
@@ -0,0 +1,60 @@
+package com.examples.operators;
+
+import java.util.Random;
+
+import static com.examples.util.Print.print;
+
+public class BitManipulation {//использование поразрядных операторов
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        int i = rand.nextInt();
+        int j = rand.nextInt();
+        printBinaryInt("-1", -1);
+        printBinaryInt("+1", +1);
+        int maxpos = 2147483647;
+        printBinaryInt("максимальное положительное", maxpos);
+        int maxneg = -2147483648;
+        printBinaryInt("максимальное отрицательное", maxneg);
+        printBinaryInt("i", i);
+        printBinaryInt("~i", ~i);
+        printBinaryInt("-i", -i);
+        printBinaryInt("j", j);
+        printBinaryInt("i & j", i & j);
+        printBinaryInt("i | j", i | j);
+        printBinaryInt("i ^ j", i ^ j);
+        printBinaryInt("i << 5", i << 5);
+        printBinaryInt("i >> 5", i >> 5);
+        printBinaryInt("(~i) >> 5", (~i) >> 5);
+        printBinaryInt("i >>> 5", i >>> 5);
+        printBinaryInt("(~i) >>> 5", (~i) >>> 5);
+
+        long l = rand.nextLong();
+        long m = rand.nextLong();
+        printBinaryLong("-1L", -1L);
+        printBinaryLong("+1L", +1L);
+        long ll = 9223372036854775807L;
+        printBinaryLong("максимальное положительное", ll);
+        long lln = -9223372036854775808L;
+        printBinaryLong("максимальное отрицательное", lln);
+        printBinaryLong("l", l);
+        printBinaryLong("~l", ~l);
+        printBinaryLong("-l", -l);
+        printBinaryLong("m", m);
+        printBinaryLong("l & m", l & m);
+        printBinaryLong("l | m", l | m);
+        printBinaryLong("l ^ m", l ^ m);
+        printBinaryLong("l << 5", l << 5);
+        printBinaryLong("l >> 5", l >> 5);
+        printBinaryLong("(~l) >> 5", (~l) >> 5);
+        printBinaryLong("l >>> 5", l >>> 5);
+        printBinaryLong("(~l) >>> 5", (~l) >>> 5);
+    }
+    static void printBinaryInt(String s, int i) {
+        print(s + ", int: " + i + ", двоичное:\n   " +
+                Integer.toBinaryString(i));
+    }
+    static void printBinaryLong(String s, long l) {
+        print(s + ", long: " + l + ", двоичное:\n    " +
+                Long.toBinaryString(l));
+    }
+}
