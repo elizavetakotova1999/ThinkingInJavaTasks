a1e8d43d5d3a5c26eedef2ae49830b0f3e5ac984
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-03 00:07:01 +0200

    added AutoInc; Page 102;

---
+++ src/com/examples/operators/AutoInc.java
@@ -0,0 +1,15 @@
+package com.examples.operators;
+import static com.examples.util.Print.print;
+
+public class AutoInc {
+    public static void main(String[] args) {
+        int i = 1;
+        print("i = " + i);
+        print("++i : " + ++i); // Префиксный инкремент
+        print("i++ : " + i++); // Постфиксный инкремент
+        print("i = " + i);
+        print("--i : " + --i); // Префиксный декремент
+        print("i-- : " + i--); // Постфиксный декремент
+        print("i : " + i);
+    }
+}
