cd38ee87aa07f910fa39f327d64de62a4bfbc14d
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-02 15:26:30 +0200

    added ShowProperties; Page 85;

---
+++ src/com/company/object/ShowProperties.java
@@ -0,0 +1,11 @@
+package com.company.object;
+
+public class ShowProperties {
+    public static void main(String[] args) {
+        System.getProperties().list(System.out);//выводит свойства системы
+        System.out.println("--------------------------------------");
+        System.out.println(System.getProperty("user.name"));//Имя пользователя компьютера
+        System.out.println("--------------------------------------");
+        System.out.println(System.getProperty("java.library.path"));
+    }
+}
