08e9442ce48b9a6bdb2233a5fb00ded17dabc214
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-02 23:34:32 +0200

    HelloDate

---
+++ src/com/company/operators/HelloDate.java
@@ -0,0 +1,12 @@
+package com.company.operators;
+
+import java.util.Date;
+
+import static com.company.util.Print.print;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        print("Hello, it's: ");
+        print(new Date());
+    }
+}
