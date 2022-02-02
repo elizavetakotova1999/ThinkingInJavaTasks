f1293b3be4bf5bdf15b8734a71888dbbc2cd2f8f
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-02 15:28:34 +0200

    added HelloDate; Page 84;

--- src/com/company/object/HelloDate.java
+++ src/com/company/object/HelloDate.java
@@ -3,6 +3,6 @@ import java.util.*;
 public class HelloDate {
     public static void main(String[] args) {
         System.out.println("Привет, сегодня: ");
-        System.out.println(new Date());
+        System.out.println(new Date()); //выводит текущие день, месяц, число, время и год.
     }
 }
