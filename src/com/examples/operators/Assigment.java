c7d230f3ef85608b4ae6d8f3587370ca6e8b2a74
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-03 00:06:49 +0200

    added Assigment; Page 98;

---
+++ src/com/examples/operators/Assigment.java
@@ -0,0 +1,20 @@
+package com.examples.operators;
+
+import static com.examples.util.Print.*;
+
+public class Assigment {//Присваивание
+    public static void main(String[] args) {
+        Tank t1 = new Tank();
+        Tank t2 = new Tank();
+        t1.level = 9;
+        t2.level = 47;
+        print("1: t1.level: " + t1.level + ", t2.level: " + t2.level);
+        t1 = t2;//синонимия(aliasing), t1 перезаписана и потеряла свою ссылку, а ее объект будет удален сборщиком мусора
+        print("2: t1.level: " + t1.level + ", t2.level: " + t2.level);
+        t1.level = 27;
+        print("3: t1.level: " + t1.level + ", t2.level: " + t2.level);
+    }
+}
+class Tank{
+    int level;
+}
