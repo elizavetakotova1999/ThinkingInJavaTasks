2df75463514d6a83b4ce18681136a5ca1c191115
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-02 16:07:55 +0200

    added Documentation3; Page 89; added comments of documentation , page 91;

--- src/com/company/object/HelloDate.java
+++ src/com/company/object/HelloDate.java
@@ -11,6 +11,7 @@ public class HelloDate {
      * @param args Массив строковых аргументов
      * @throws exceptions Исключения не выдаются
      */
+
     public static void main(String[] args) {
         System.out.println("Привет, сегодня: ");
         System.out.println(new Date()); //выводит текущие день, месяц, число, время и год.
