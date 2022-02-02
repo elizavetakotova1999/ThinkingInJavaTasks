1bc8cf1322a05cd1c7bb8f83a8dc31e26e1f7b2e
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-02 15:13:12 +0200

    Initial commit

---
+++ src/com/company/object/HelloDate.java
@@ -0,0 +1,8 @@
+package com.company.object;
+import java.util.*;
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Привет, сегодня: ");
+        System.out.println(new Date());
+    }
+}
