fb7b5e4f6f98f602acf924aa0ab48055bb3605b7
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-03 00:11:07 +0200

    added Exponents; Page 108;

---
+++ src/com/examples/operators/Exponents.java
@@ -0,0 +1,13 @@
+package com.examples.operators;
+
+public class Exponents {
+    public static void main(String[] args) {
+        // Прописная и строчная буквы 'e' эквивалентны:
+        float expFloat = 1.39e-43f;
+        expFloat = 1.39E-43f;
+        System.out.println(expFloat);
+        double expDouble = 47e47d; // суффикс 'd' не обязателен
+        double expDouble2 = 47e47; // Автоматически определяется double
+        System.out.println(expDouble);
+    }
+}
