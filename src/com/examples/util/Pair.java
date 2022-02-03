7688618b166758947d25028c07168469fc5c5854
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-04 01:10:05 +0200

    added class Pair to util;

---
+++ src/com/examples/util/Pair.java
@@ -0,0 +1,10 @@
+package com.examples.util;
+
+public class Pair<K,V> {
+    public final K key;
+    public final V value;
+    public Pair(K k, V v) {
+        key = k;
+        value = v;
+    }
+}
