1de6b4f75af26df76a6946c3f1bd778524c6b99b
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-04 00:38:50 +0200

    added class DaemonThreadPoolExecutor to util;

---
+++ src/com/examples/util/DaemonThreadPoolExecutor.java
@@ -0,0 +1,11 @@
+package com.examples.util;
+
+import java.util.concurrent.*;
+
+public class DaemonThreadPoolExecutor extends ThreadPoolExecutor {
+    public DaemonThreadPoolExecutor() {
+        super(0, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>(),
+                new DaemonThreadFactory());
+    }
+}
