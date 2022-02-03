96f944fa4c0b356936d5eeb172037011897b612b
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-04 00:40:09 +0200

    added class Deque to util;

---
+++ src/com/examples/util/Deque.java
@@ -0,0 +1,15 @@
+package com.examples.util;
+import java.util.*;
+
+public class Deque<T> {
+    private LinkedList<T> deque = new LinkedList<T>();
+    public void addFirst(T e) { deque.addFirst(e); }
+    public void addLast(T e) { deque.addLast(e); }
+    public T getFirst() { return deque.getFirst(); }
+    public T getLast() { return deque.getLast(); }
+    public T removeFirst() { return deque.removeFirst(); }
+    public T removeLast() { return deque.removeLast(); }
+    public int size() { return deque.size(); }
+    public String toString() { return deque.toString(); }
+    // И другие методы по мере необходимости...
+}
