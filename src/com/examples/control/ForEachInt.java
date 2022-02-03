fc2c2463a8ff0699f00e5fbfa16b448cc37e1a56
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-03 23:45:47 +0200

    added ForEachInt; Page 133;

---
+++ src/com/examples/control/ForEachInt.java
@@ -0,0 +1,24 @@
+package com.examples.control;
+
+import static com.examples.util.Range.*;//методы с реализацией диапазона(range()),через массивы. Методы перегружены
+import static com.examples.util.Print.*;
+
+public class ForEachInt {
+    public static void main(String[] args) {
+        //Любой метод, возвращающий массив, может использоваться с foreach
+        for (int i:range(10)) {//0..9
+            printnb(i + " ");
+           // print();
+        }
+        print();
+        for (int i: range(5,10)) {//5..9
+            printnb(i + " ");
+            //print();
+        }
+        print();
+        for (int i: range(5,20,3)) {//5..20 шаг 3
+            printnb(i + " ");
+           // print();
+        }
+    }
+}
