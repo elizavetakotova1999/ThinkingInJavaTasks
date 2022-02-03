114479dde7d1e6ce50bd82c39ffcf60c736a2f76
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-04 01:55:16 +0200

    added class SwingConsole to util;

---
+++ src/com/examples/util/SwingConsole.java
@@ -0,0 +1,15 @@
+package com.examples.util;
+import javax.swing.*;
+
+public class SwingConsole {
+    public static void run(final JFrame f, final int width, final int height) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                f.setTitle(f.getClass().getSimpleName());
+                f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                f.setSize(width, height);
+                f.setVisible(true);
+            }
+        });
+    }
+}
