abc980c19cbc539a0790188c51a291f53d128175
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-04 01:48:01 +0200

    added class ProcessFiles to util;

---
+++ src/com/examples/util/ProcessFiles.java
@@ -0,0 +1,54 @@
+package com.examples.util;
+
+import java.io.*;
+
+public class ProcessFiles {
+    private final Strategy strategy;
+    private final String ext;
+    public ProcessFiles(Strategy strategy, String ext) {
+        this.strategy = strategy;
+        this.ext = ext;
+    }
+
+    // Демонстрация использования:
+    public static void main(String[] args) {
+        new ProcessFiles(new ProcessFiles.Strategy() {
+            public void process(File file) {
+                System.out.println(file);
+            }
+        }, "java").start(args);
+    }
+
+    public void start(String[] args) {
+        try {
+            if (args.length == 0)
+                processDirectoryTree(new File("."));
+            else
+                for (String arg : args) {
+                    File fileArg = new File(arg);
+                    if (fileArg.isDirectory())
+                        processDirectoryTree(fileArg);
+                    else {
+                        // Разрешить пользователю отключить расширение:
+                        if (!arg.endsWith("." + ext))
+                            arg += "." + ext;
+                        strategy.process(
+                                new File(arg).getCanonicalFile());
+                    }
+                }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void
+    processDirectoryTree(File root) throws IOException {
+        for (File file : Directory.walk(
+                root.getAbsolutePath(), ".*\\." + ext))
+            strategy.process(file.getCanonicalFile());
+    }
+
+    public interface Strategy {
+        void process(File file);
+    }
+}
