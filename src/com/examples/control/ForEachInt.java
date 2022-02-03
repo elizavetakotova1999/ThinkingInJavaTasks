efed2eb7f566389318bfb3c769c9ca2f79579d34
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-03 23:46:22 +0200

    added ForEachInt; Page 133;

--- src/com/examples/control/ForEachInt.java
+++ src/com/examples/control/ForEachInt.java
@@ -8,17 +8,14 @@ public class ForEachInt {
         //Любой метод, возвращающий массив, может использоваться с foreach
         for (int i:range(10)) {//0..9
             printnb(i + " ");
-           // print();
         }
         print();
         for (int i: range(5,10)) {//5..9
             printnb(i + " ");
-            //print();
         }
         print();
         for (int i: range(5,20,3)) {//5..20 шаг 3
             printnb(i + " ");
-           // print();
         }
     }
 }
