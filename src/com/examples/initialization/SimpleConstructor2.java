71aad552ba074ff539870a5a8d4ccd08f0c0c0fd
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-05 00:02:59 +0200

    added SimpleConstructor2; Page 145;

---
+++ src/com/examples/initialization/SimpleConstructor2.java
@@ -0,0 +1,15 @@
+package com.examples.initialization;
+
+
+class Rock2{
+    Rock2(int i){
+        System.out.print("Rock " + i + " ");
+    }
+}
+public class SimpleConstructor2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 8; i++) {
+            new Rock2(i);
+        }
+    }
+}
