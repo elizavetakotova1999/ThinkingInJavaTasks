db3bc9446d971a8d11168643a01efbb906aeaddd
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-04 02:01:07 +0200

    added class TaskManager to util;

---
+++ src/com/examples/util/TaskManager.java
@@ -0,0 +1,46 @@
+package com.examples.util;
+
+import java.util.concurrent.*;
+import java.util.*;
+// Управление и выполнение очереди задач./Managing and executing a queue of tasks.
+
+public class TaskManager<R, C extends Callable<R>>
+        extends ArrayList<TaskItem<R, C>> {
+    private ExecutorService exec = Executors.newSingleThreadExecutor();
+
+    public void add(C task) {
+        add(new TaskItem<R, C>(exec.submit(task), task));
+    }
+
+    public List<R> getResults() {
+        Iterator<TaskItem<R, C>> items = iterator();
+        List<R> results = new ArrayList<R>();
+        while (items.hasNext()) {
+            TaskItem<R, C> item = items.next();
+            if (item.future.isDone()) {
+                try {
+                    results.add(item.future.get());
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                items.remove();
+            }
+        }
+        return results;
+    }
+
+    public List<String> purge() {
+        Iterator<TaskItem<R, C>> items = iterator();
+        List<String> results = new ArrayList<String>();
+        while (items.hasNext()) {
+            TaskItem<R, C> item = items.next();
+            // Оставляем выполненные задачи для отчета по результатам:
+            if (!item.future.isDone()) {
+                results.add("Cancelling " + item.task);
+                item.future.cancel(true); // Может прерывать
+                items.remove();
+            }
+        }
+        return results;
+    }
+}
