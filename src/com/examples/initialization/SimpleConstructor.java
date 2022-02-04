e11e65f79b767d274c2c78d65bc1e303a818a1d2
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-04 23:59:08 +0200

    added SimpleConstructor; Page 144;

---
+++ src/com/examples/initialization/SimpleConstructor.java
@@ -0,0 +1,14 @@
+package com.examples.initialization;
+
+class Rock{
+  Rock(){
+      System.out.print("Rock ");
+  }
+}
+public class SimpleConstructor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Rock();
+        }
+    }
+}
