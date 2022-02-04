866348585417fc64a024ab10798fc9d63ce779be
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-04 19:43:54 +0200

    added ifElse2; Page 134;

---
+++ src/com/examples/control/ifElse2.java
@@ -0,0 +1,21 @@
+package com.examples.control;
+
+import static com.examples.util.Print.print;
+
+public class ifElse2 {
+    static int test(int testval, int target){
+        if (testval>target) {
+            return +1;
+        } else if (testval< target) {
+            return -1;
+        } else {
+            return 0;//одинаковые значения
+        }
+    }
+
+    public static void main(String[] args) {
+        print(test(10, 5));
+        print(test(5,10));
+        print(test(5,5));
+    }
+}
