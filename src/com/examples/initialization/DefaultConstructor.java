325fa13413c9bd92441413e8f854b93abb8746a7
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-05 22:46:18 +0200

    added DefaultConstructor; Page 152;

---
+++ src/com/examples/initialization/DefaultConstructor.java
@@ -0,0 +1,8 @@
+package com.examples.initialization;
+
+class Bird{}
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        Bird b = new Bird();//По умолчанию
+    }
+}
