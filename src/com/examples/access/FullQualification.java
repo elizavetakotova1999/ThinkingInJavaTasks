8a08ad939dd2f26e748a2cc6c9a0da719a3026ab
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-06 02:59:05 +0200

    added FullQualification; Page 187;

---
+++ src/com/examples/access/FullQualification.java
@@ -0,0 +1,7 @@
+package com.examples.access;
+
+public class FullQualification {
+    public static void main(String[] args) {
+        java.util.ArrayList list = new java.util.ArrayList();//полное имя пакета
+    }
+}
