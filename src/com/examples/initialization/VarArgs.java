07e8768592af874d860c1dd1550fb40725e59df3
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-06 02:31:40 +0200

    added VarArgs; Page 177-178;

---
+++ src/com/examples/initialization/VarArgs.java
@@ -0,0 +1,18 @@
+package com.examples.initialization;
+
+//Использование синтаксиса массивов для получения переменного списка аргументов
+
+class A {}
+
+public class VarArgs {
+    static void printArray(Object[] args) {
+        for(Object obj : args)
+            System.out.print(obj + " ");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        printArray(new Object[]{ new Integer(47), new Float(3.14), new Double(11.11)});
+        printArray(new Object[]{"one", "two", "three" });
+        printArray(new Object[]{new A(), new A(), new A()});
+    }
+}
