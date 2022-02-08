2c10c3ef25bf3a591e0ed4b23ee6fcc4a00f7c65
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-08 22:44:58 +0200

    added ImportedMyClass; Page 189;

---
+++ src/com/examples/access/ImportedMyClass.java
@@ -0,0 +1,9 @@
+package com.examples.access;
+
+import com.examples.access.mypackage.*;
+
+public class ImportedMyClass {
+    public static void main(String[] args) {
+        MyClass m = new MyClass();
+    }
+}
