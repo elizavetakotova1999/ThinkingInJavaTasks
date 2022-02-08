a2f487b3d60f0fa2782c794a89b9d78b8d0d2815
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-08 22:43:05 +0200

    added QualifiedMyClass; Page 189;

---
+++ src/com/examples/access/QualifiedMyClass.java
@@ -0,0 +1,10 @@
+package com.examples.access;
+
+import com.examples.access.mypackage.MyClass;
+
+public class QualifiedMyClass {
+    public static void main(String[] args) {
+        MyClass m = new MyClass();
+
+    }
+}
