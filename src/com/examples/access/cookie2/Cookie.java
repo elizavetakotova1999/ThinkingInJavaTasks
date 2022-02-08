b58108e2384ec5c90b27a0817221b3fed93de068
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-08 23:42:49 +0200

    added Cookie; Page 200;

---
+++ src/com/examples/access/cookie2/Cookie.java
@@ -0,0 +1,10 @@
+package com.examples.access.cookie2;
+
+public class Cookie {
+    public Cookie() {
+        System.out.println("Cookie конструктор");
+    }
+    protected void bite() {
+        System.out.println("bite");
+    }
+}
