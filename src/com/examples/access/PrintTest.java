0993ddb7f78714c3dfa84bdb9f36baa8762a13bf
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-08 22:57:46 +0200

    added PrintTest; Page 193;

---
+++ src/com/examples/access/PrintTest.java
@@ -0,0 +1,12 @@
+package com.examples.access;
+
+import static com.examples.util.Print.*;
+
+public class PrintTest {
+    public static void main(String[] args) {
+        print("Теперь это стало возможно!");
+        print(100);
+        print(100L);
+        print(3.14159);
+    }
+}
