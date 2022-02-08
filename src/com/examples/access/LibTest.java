b057524c70b47ad9381114a2117eebbf63d79ece
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-08 22:53:41 +0200

    added LibTest; Page 191-192;

---
+++ src/com/examples/access/LibTest.java
@@ -0,0 +1,10 @@
+package com.examples.access;
+
+import com.examples.simple.*;
+
+public class LibTest {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+        List l = new List();
+    }
+}
