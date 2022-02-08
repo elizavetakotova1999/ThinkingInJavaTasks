1c0efde48b97db9170c5a55a676c4d1fb10233fa
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-08 23:08:57 +0200

    added Dinner; Page 197;

---
+++ src/com/examples/access/Dinner.java
@@ -0,0 +1,10 @@
+package com.examples.access;
+
+import com.examples.access.dessert.Cookie;
+
+public class Dinner {
+    public static void main(String[] args) {
+        Cookie x = new Cookie();
+        //! x.bite(); // Обращение невозможно
+    }
+}
