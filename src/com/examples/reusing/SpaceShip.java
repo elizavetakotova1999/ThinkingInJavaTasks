150cb8e32f67158d228043c10ea0675fdafd147a
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-12 01:44:17 +0200

    added SpaceShipControls; Page 214;

---
+++ src/com/examples/reusing/SpaceShip.java
@@ -0,0 +1,18 @@
+package com.examples.reusing;
+//Для построения космического корабля можно воспользоваться наследованием
+public class SpaceShip extends SpaceShipControls {
+    private final String name;
+
+    public SpaceShip(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        SpaceShip protector = new SpaceShip("NSEA Protector");
+        protector.forward(100);
+    }
+}
