61001bd751f2c90839e217778e4d0a687677ac1a
elizavetakotova1999 <kotakoray@gmail.com>
2022-02-12 01:40:53 +0200

    added SpaceShipControls; Page 214;

--- src/com/examples/reusing/Chess.java
+++ src/com/examples/reusing/Chess.java
@@ -1,24 +1,24 @@
 package com.examples.reusing;
-
+//Наследование, конструкторы и аргументы
 import static com.examples.util.Print.print;
 
 class Game {
     Game(int i) {
-        print("Game constructor");
+        print("Конструктор Game");
     }
 }
 
 class BoardGame extends Game {
     BoardGame(int i) {
         super(i);//Вызов базового класса
-        print("BoardGame constructor");
+        print("Конструктор BoardGame");
     }
 }
 
 public class Chess extends BoardGame {
     Chess() {
         super(11);//Вызов базового класса
-        print("Chess constructor");
+        print("Конструктор Chess");
     }
 
     public static void main(String[] args) {
---
+++ src/com/examples/reusing/SpaceShipControls.java
@@ -0,0 +1,12 @@
+package com.examples.reusing;
+//Делегирование
+//Имитирует модуль управления космическим кораблем
+public class SpaceShipControls {
+    void up(int velocity) {}
+    void down(int velocity) {}
+    void left(int velocity) {}
+    void right(int velocity) {}
+    void forward(int velocity) {}
+    void back(int velocity) {}
+    void turboBoost() {}
+}
